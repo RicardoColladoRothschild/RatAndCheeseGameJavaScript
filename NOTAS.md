1c8b7ce072e4387fbbd1c3288cf4b981c9afb92d
ricardocollado <ricardocolladorothschild@gmail.com>
2023-10-14 01:57:01 -0400

    trying to fill the whole canvas with the emoji

---
+++ NOTAS.md
@@ -0,0 +1,21 @@
+# Reto del modulo "Array Bidimencional
+Task:
+Los mapas son 10x19, un arreglo bidimencional de 10 elementos cada uno con 10 elementos. 
+Cada mapa va a ser un arreglo de filas. 
+Ten en cuenta, te entregan un array, normal, que contiene, un string largo... pro elemento, 
+lo que vamos a hacer es, tomar una posicion x, luego convertir esa poscion (Que seria un mapa)
+y convertirlo a un arreglo bidimencional, por columnas:
+    0 1 2 3 4 5 6 7 8 9 ...
+0 | x x x x x x x x x x
+1 | x x x x x x x x x x
+2 | x x x x x x x x x x 
+3 | x x x x x x x x x x 
+4 | x x x x x x x x x x
+5 | x x x x x x x x x x 
+
+Como puedes ver esto dereria ser el resultado, de tu arreglo, al final, claro tendras varios mapas, 
+donde x estara en un lugar distinto y etc...etc...etc..
+Recordando que x, dentro de un objeto de emojis, representa una emonji, y asi
+sucesivamente cada uno de los otros caracteres. 
+El reto es llenar de emojis el canvas
+clave: usando 2 cilos gfor de complejidad o(n^2)
--- main.js
+++ main.js
@@ -2,13 +2,10 @@ const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d'); // nos referimos a que van a haber 2 ejes, eje x y eje y.
 let canvasSize;
 let elementsSize;
-window.addEventListener('load', startGame);
+window.addEventListener('load', setCanvasSize);
 
-//there was a problem when loading the website,if it resize, it did not respond responsive to the change, had to be reload it to it
+
 window.addEventListener('resize',setCanvasSize);
-
-//esta funcion es que va a cargar todo lo que vayamos a usar. 
-
 
 function setCanvasSize(){
   canvasSize = Math.min(window.innerWidth, window.innerHeight) * 0.8;
@@ -23,12 +20,17 @@ function setCanvasSize(){
 function startGame(){
 
   //setCanvasSize();
-
+  
    
-    //console.log(`canvasSize: ${canvasSize} | elements size: ${elementsSize}`);
     game.font = elementsSize +'px Arial';
     game.textAlign = 'center';
-   for(let i = 1; i < 10; i++){
-     game.fillText(emojis['X'],elementsSize * i, elementsSize);
-   }   
+    
+    for(let i = 1; i < 10; i++){
+
+     game.fillText(emojis['X'],elementsSize, elementsSize  * i);
+      for(let j = 2; j < 10; j++){
+        game.fillText(emojis['X'],elementsSize * j, elementsSize);
+      }
+      
+    }   
 }
