871c48259b15cd053c844ea26ca84c9a68a712f2
ricardocollado <ricardocolladorothschild@gmail.com>
2023-10-15 14:56:42 -0400

    enhance the code

--- main.js
+++ main.js
@@ -27,20 +27,30 @@ function startGame(){
     
     const map = maps[0];
     console.log(maps[0]);
-    const mapRows = maps[1].trim().split('\n'); 
+    const mapRows = maps[0].trim().split('\n'); 
     console.log(mapRows);
 
     const mapRowCols = mapRows.map(row=>row.trim().split(''));
     console.log(mapRowCols);
 
+    mapRowCols.forEach((rows, rowIndx)=>{
+       rows.forEach((col, colIndx)=>{
+        const emoji = emojis[col];
+        const posX = elementsSize * (colIndx + 1);
+        const posY = elementsSize * (rowIndx + 1);
+
+        game.fillText(emoji, posX, posY);
+      });
+    });
+
     //para completar este ejercicio, hemos multiplicado las coordenadas por el elementsize, para
     //de esta forma ir obteniendo la nueva posicion del elemento.
-    for(let row = 1; row <= 10; row++){
-      for(let col = 1; col <= 10; col++){
+    // for(let row = 1; row <= 10; row++){
+    //   for(let col = 1; col <= 10; col++){
         
-          game.fillText(emojis[mapRowCols[row-1][col-1]],elementsSize * col, elementsSize * row);
+    //       game.fillText(emojis[mapRowCols[row-1][col-1]],elementsSize * col, elementsSize * row);
               
-      }
+    //   }
       
-    }   
+    // }   
 }
